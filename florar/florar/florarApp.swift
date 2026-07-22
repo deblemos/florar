517075e83c706340ac59866fff2cc3cd41a4aa5e
Débora Lemos <delemos004@gmail.com>
2026-07-22 14:14:23 -0300

    Initialize SwiftUI project

---
+++ florar/florar/florarApp.swift
@@ -0,0 +1,17 @@
+//
+//  florarApp.swift
+//  florar
+//
+//  Created by Débora Lemos on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct florarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
